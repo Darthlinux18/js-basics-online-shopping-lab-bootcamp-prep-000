a11c1bda26bd5964f0168d6c0dfdc54f44852efd
craig sperlazza <craig.sperlazza@gmail.com>
2018-01-08 17:30:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/*ES 6 provides a way to use variables as object keys — you have to wrap the key in square brackets ([]).
+Using the above example, you could write var meals = { [firstMeal]: 'oatmeal' }
+and then meals would be { breakfast: 'oatmeal' } */
+
+
 var cart = [];
 
 function getCart() {
@@ -10,19 +15,54 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var newPrice = Math.floor(99*Math.random() +1);
+  var newItem = `${item}`;
+  cart.push(new Object({[newItem]: newPrice}));
+
+  console.log(`${item} has been added to your cart.`);
+
+  return cart;
+
 }
 
 function viewCart() {
-  // write your code here
+  if(cart.length > 0) {
+      if (cart.length === 1) {
+        console.log("In your cart, you have " + Object.keys(cart[0]) + " at $" + cart[0][Object.keys(cart[0])] +".");
+      }
+
+      if (cart.length === 2) {
+          console.log("In your cart, you have " + Object.keys(cart[0]) + " at $" + cart[0][Object.keys(cart[0])] +" and " + Object.keys(cart[1]) + " at $" + cart[1][Object.keys(cart[1])] + ".");
+        }
+
+        if (cart.length >= 3) {
+          var itemList = [];
+          for (let i = 0; i < cart.length; i++) {
+            itemList.push(Object.keys(cart[i]) + " at $" + cart[i][Object.keys(cart[i])]);
+       //  console.log(itemList);
+          }
+          var last = itemList.pop();
+          console.log("In your cart, you have " + itemList.join(", ") + ", and " + last + ".");
+      }
+
+  } else {
+      console.log("Your shopping cart is empty.");
+    }
 }
 
+
 function total() {
-  // write your code here
+  var sum = 0;
+  for(var i = 0; i < cart.length; i++) {
+    var item = Object.keys(cart[i])[0];
+    var price = cart[i][item];
+    sum += price;
+  }
+  return sum;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  
 }
 
 function placeOrder(cardNumber) {
